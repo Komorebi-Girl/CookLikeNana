6f75152bea4fc3e996ec2ea2eafcfe78e0535109
Erin Rizal <erinrizal@Erins-MacBook-Pro.local>
2018-02-23 16:07:02 -0500

    hamburger

--- src/App.js
+++ src/App.js
@@ -7,13 +7,27 @@ import Landing from "./components/Landing/Landing";
 import NavBar from "./components/NavBar/NavBar";
 import Section1 from "./components/Landing/Section1";
 import Section2 from "./components/Landing/Section2";
+import Col from 'react-bootstrap/lib/Col';
+import Hamburger from './components/Landing/hamburger'
+import { slide as Menu } from '../node_modules/react-burger-menu'
 
 const App = () =>
     <div>
+      {/* <div id="outer-container">
+  <Menu pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" } />
+  <main id="page-wrap">
+    .
+    .
+    .
+  </main>
+</div> */}
+  
+      <Hamburger />
       <NavBar />
       <Landing />
       <Section1 />
       <Section2 />
+     
     </div>
     
 export default App;
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,8 +2,7 @@ import React from "react";
 import "./Landing.css";
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
-// import './slanted.scss';
+import { slide as Menu } from 'react-burger-menu'
 
 const Landing = props => (
 	<div>
@@ -16,8 +15,6 @@ const Landing = props => (
 			<h4>	Lost college freshmen. Broke Milienials. Stressed out newly weds. When it comes to cooking, a lot of people don't know where to start. 
              That's why we set up Cook With Nana: to help new cooks gain the knowledge to cook like pros, and nothing beats an authentic homemade meal from grandma.	 
 						 </h4>
-					
-	
 </div>
 
 	</div>
