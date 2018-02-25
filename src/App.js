f591c17023d8895eb5a6bb33bd808d8e4af897de
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2018-02-25 00:26:08 -0500

    profile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import SearchPage from "./pages/SearchPage";
@@ -9,21 +8,12 @@ import NanaRegister from "./components/NanaRegister";
 import UserRegister from "./components/UserRegister";
 import UserProfile from "./components/UserProfile";
 import LoginPage from "./pages/LoginPage";
-=======
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NanaRegister from "./components/NanaRegister/NanaRegister";
-import UserRegister from "./components/UserRegister/UserRegister";
-import Landing from "./pages/Landing/Landing";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
-import SearchPage from "./pages/SearchPage/SearchPage";
->>>>>>> e8136e1df62fb50fb4d7aa1d49382ae81216364f
 
 // authenticate = userInfo => {
 //   this.setState({userInfo})
 // }
 
 const App = () =>
-<<<<<<< HEAD
   <Router>
     <div>
       <Route exact path="/" component={LoginPage} />
@@ -33,22 +23,6 @@ const App = () =>
       <Route path="/userprofile/:id" component={UserProfile} />
     </div>
   </Router>
-=======
-    <Router>
-    	<div>
-	    	<Route exact path="/" component={Landing} />
-	    	<Route exact path="/profile" component={ProfilePage} />
-	    	<Route exact path="/search" component={SearchPage} />
-	    	<Route exact path="/nanaregister" component={NanaRegister} />
-	    	<Route exact path="/userregister" component={UserRegister} />
-	    	
-
-
-
-
-    	</div>
-    </Router>
->>>>>>> e8136e1df62fb50fb4d7aa1d49382ae81216364f
     
 export default App;
 
