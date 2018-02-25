a53ac4b861ecbea5a62ac011dc7480572ed36510
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2018-02-24 23:27:23 -0500

    added functioning profile pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import SearchPage from "./pages/SearchPage";
 // import NoMatch from "./components/NoMatch";
-// import NanaRegister from "./components/NanaRegister/NanaRegister";
-import UserRegister from "./components/UserRegister/UserRegister";
-// import LoginPage from "./pages/LoginPage";
+import NanaProfile from "./components/NanaProfile";
+import NanaRegister from "./components/NanaRegister";
+import UserRegister from "./components/UserRegister";
+import UserProfile from "./components/UserProfile";
+import LoginPage from "./pages/LoginPage";
 
+// authenticate = userInfo => {
+//   this.setState({userInfo})
+// }
 
 const App = () =>
+  <Router>
     <div>
-      {/* <LoginPage /> */}
-      {/* <NanaRegister /> */}
-      <UserRegister />
+      <Route exact path="/" component={LoginPage} />
+      <Route path="/nana_register" component={NanaRegister} />
+      <Route path="/user_register" component={UserRegister} />
+      <Route path="/nanaprofile/:id" component={NanaProfile} />
+      <Route path="/userprofile/:id" component={UserProfile} />
     </div>
+  </Router>
     
 export default App;
 
---
+++ src/components/UserProfile/UserProfile.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import "./Profile.css";
+import API from "../../utils/API";
+
+class UserProfile extends Component {
+  state = {
+    fullname: "NewUser",
+    location: "Somewhere, FL",
+    image: "http://unmr-nl.science.uu.nl/sites/default/files/user_placeholder_man_0.jpg",
+    preferences: "Italian",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab minima, repudiandae ex explicabo distinctio praesentium magnam velit inventore optio, dignissimos fuga, ipsum quos vero consequuntur! Voluptate cumque culpa eum nisi!"
+
+  }
+
+ componentDidMount(){
+   API.grabUserData().then(res => {
+     console.log(res)
+    this.setState({firstname: res.data.fullname, location: res.data.location, bio: res.data.bio, image: res.data.profileimage})
+   })
+ } 
+
+  render(){
+    return(	<div className="container">
+		<div className="row">
+
+			<div class="col-md-4">
+				<div class="thumbnail">
+      				<img src={this.state.image} alt="user"></img>
+    		</div>
+		  </div>
+
+			<div class="col-md-8">
+				<h1>{this.state.fullname}</h1>
+				<p>{this.state.location}</p>
+				<h4>Cuisine Preferences:</h4>
+				<p>{this.state.specialties}</p>
+				<h4>About Me:</h4>
+				<p>{this.state.bio}</p>
+			</div>
+
+		</div>
+	</div>)}  
+};
+
+export default UserProfile;
