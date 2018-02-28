bf8dbcd301f0c534a40f75fc1e4f14b3da6339b0
Erin Rizal <erinrizal@Erins-MacBook-Pro.local>
2018-02-28 00:37:14 -0500

    signif. change on page appearences

--- src/pages/SchedulePage/SchedulePage.js
+++ src/pages/SchedulePage/SchedulePage.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import ApptCard from "../../components/ApptCard";
 import Container from "../../components/Container";
+import PreComponent from "../../components/PreComponent/PreComponent";
 
 class SchedulePage extends Component {
 
   render() {
     return (
-      <Container fluid>
+      <div>
+
+
         <ApptCard/>
-      </Container>
+        </div>
     );
   }
 }
