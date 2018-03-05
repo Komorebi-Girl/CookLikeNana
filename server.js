869dc1746ec08d2e2fa4bf8a3c8207d52a0b2654
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2018-03-05 12:57:57 -0500

    added res.send back in with final slash

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.use(passport.session());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, './build/')));
-  // app.get('/', function (req, res) {
-  //   res.sendFile(path.join(__dirname, './build', 'index.html'));
-  // });
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './build/', 'index.html'));
+  });
   app.use('/login', express.static(path.join(__dirname, './build/')));
   app.use('/nanas', express.static(path.join(__dirname, './build/')));
   app.use('/nana', express.static(path.join(__dirname, './build/')));
