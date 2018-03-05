c30200ac68b613780a1f73b8d284d101c81ad0bb
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2018-03-05 07:46:24 -0500

    fix display eight attempt

--- server.js
+++ server.js
@@ -14,22 +14,22 @@ const passport = require("./config/passport");
 app.use(morgan());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// Readying app for production
+
+app.use(express.static(path.join(__dirname, './build')));
+app.use('/login', express.static(path.join(__dirname, './build'))));
+app.use('/nanas', express.static(path.join(__dirname, './build'))));
+app.use('/nana', express.static(path.join(__dirname, './build'))));
+app.use('/user', express.static(path.join(__dirname, './build'))));
+app.use('/nana/:id', express.static(path.join(__dirname, './build'))));
+app.use('/user/:id', express.static(path.join(__dirname, './build'))));
+app.use('/schedule', express.static(path.join(__dirname, './build'))));
 
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
-
-app.get('/test',(req,res) => {
-  if (req.user) return res.send({Hi:"There"});
-  res.send({message: 'You\' not suppose to be here you filthy animal!'})
-});
 
 app.use(routes);
 
