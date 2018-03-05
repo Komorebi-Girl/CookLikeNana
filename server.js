2e0e793bd115d31c0d855c96c314bfa2b1525ac1
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2018-03-05 14:31:06 -0500

    display static folder on root

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(passport.session());
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/static', express.static(path.join(__dirname, './build/static')));
+  app.use('/static', express.static(path.join(__dirname, '../build/static')));
   // app.use(express.static(path.join(__dirname, './build/')));
   app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './build/', 'index.html'));
