b2ce61b4718300824c4ee0e967116fbb0a3fd57d
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2018-03-05 21:32:33 -0500

    took static route out of conditional

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/static', express.static(path.join(__dirname, './build/static')));
+app.use('/login', express.static(path.join(__dirname, './build/static')));
+
 
 if (process.env.NODE_ENV =='production') {
   app.use('/static', express.static(path.join(__dirname, './build/static')));
