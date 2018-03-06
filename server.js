605c3f87d14f5b73be02e3476a729987a5516b73
Priya Polla <priyapolla@Priyas-MacBook-Pro.local>
2018-03-05 21:41:27 -0500

    yet another try

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(passport.session());
 if (process.env.NODE_ENV === 'production') {
   app.use('/static', express.static(path.join(__dirname, './build/static')));
   app.use(express.static(path.join(__dirname, './build')));
-    app.get('/', function (req, res) {
+    app.get('*', function (req, res) {
       res.sendFile(path.join(__dirname, './build', 'index.html'));
     });
   app.use('/login', express.static(path.join(__dirname, './build')));
