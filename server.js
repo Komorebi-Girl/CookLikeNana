e304cdff4f628ba8a80210611323f87133fc1746
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2018-03-05 22:33:28 -0500

    added another static folder

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/static', express.static(path.join(__dirname, 'build')));
-app.use('/login', express.static(path.join(__dirname, 'build/static')));
+app.use('/login', express.static(path.join(__dirname, 'build')));
 
 
 if (process.env.NODE_ENV =='production') {
