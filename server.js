240982f2be1c53baec690c7e9948de3df61cb05c
Priya Polla <priyapolla@Priyas-MacBook-Pro.local>
2018-03-05 22:23:38 -0500

    *

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ app.use(passport.session());
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/static', express.static(path.join(__dirname, './build/static')));
-  app.use(express.static(path.join(__dirname, './build')));
+  app.use('/static', express.static(path.join(__dirname, '../build/static')));
+  app.use(express.static(path.join(__dirname, '../build')));
     app.get('/', function (req, res) {
-      res.sendFile(path.join(__dirname, './build/', 'index.html'));
+      res.sendFile(path.join(__dirname, '../build/', 'index.html'));
     });
   app.use('/login', express.static(path.join(__dirname, './build')));
   app.use('/nanas', express.static(path.join(__dirname, './build')));
