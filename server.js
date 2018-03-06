f9b1fef3c0be509b70c7200f6c52f5288bd0bd34
Priya Polla <priyapolla@Priyas-MacBook-Pro.local>
2018-03-05 21:27:55 -0500

    another last try

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use('/static', express.static(path.join(__dirname, './build/static')));
   app.use(express.static(path.join(__dirname, './build')));
     app.get('/', function (req, res) {
-      res.sendFile(path.join(__dirname, './build/', 'index.html'));
+      res.sendFile(path.join(__dirname, './build', 'index.html'));
     });
   app.use('/login', express.static(path.join(__dirname, './build')));
   app.use('/nanas', express.static(path.join(__dirname, './build')));
