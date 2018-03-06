a5cab053c2a95ddba09ca254eed0ebacee79d9eb
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2018-03-06 18:05:40 -0500

    added in res.send again

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ app.use(passport.session());
 
 // app.use('/static', express.static(path.join(__dirname, 'build')));
 app.use(express.static(path.join(__dirname, './build')));
-
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './build/', 'index.html'));
+  });
 
 if (process.env.NODE_ENV =='production') {
   // app.use('/static', express.static(path.join(__dirname, './build/static')));
