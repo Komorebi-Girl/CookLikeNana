8f3cb1e3ed1d617040e20c759819029a9fa9c9ea
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2018-03-07 16:42:27 -0500

    add additional routes

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use('/', express.static(path.join(__dirname, 'build/static')));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './build/', 'index.html'));
   });
+app.use('/login', express.static(path.join(__dirname, './build/')));
 
 if (process.env.NODE_ENV =='production') {
   // app.use('/static', express.static(path.join(__dirname, './build/static')));
