3e83347cab6030c33991b057b3074361a3163ae0
Erin Rizal <erinrizal@Erins-MacBook-Pro.local>
2018-03-09 14:03:15 -0500

    react-create-app on Heroku

--- server.js
+++ server.js
@@ -16,20 +16,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Readying app for production
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, './build')));
-//   app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//   });
-//   app.use('/login', express.static(path.join(__dirname, './build')));
-//   app.use('/nanas', express.static(path.join(__dirname, './build')));
-//   app.use('/nana', express.static(path.join(__dirname, './build')));
-//   app.use('/user', express.static(path.join(__dirname, './build')));
-//   app.use('/nana/:id', express.static(path.join(__dirname, './build')));
-//   app.use('/user/:id', express.static(path.join(__dirname, './build')));
-//   app.use('/schedule', express.static(path.join(__dirname, './build')));
-//   ;
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, './build')));
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+  app.use('/login', express.static(path.join(__dirname, './build')));
+  app.use('/nanas', express.static(path.join(__dirname, './build')));
+  app.use('/nana', express.static(path.join(__dirname, './build')));
+  app.use('/user', express.static(path.join(__dirname, './build')));
+  app.use('/nana/:id', express.static(path.join(__dirname, './build')));
+  app.use('/user/:id', express.static(path.join(__dirname, './build')));
+  app.use('/schedule', express.static(path.join(__dirname, './build')));
+  ;
+}
 
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
