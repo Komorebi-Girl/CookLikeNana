63e1d9f91bebca9e7d2cba4b74a1cc2953a26f3c
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2018-03-17 22:09:27 -0400

    fixed typos

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,30 @@
-import React, {Component} from "react";
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Row';
-import Grid from 'react-bootstrap/lib/Grid';
+import React, { Component } from "react";
+import Row from "react-bootstrap/lib/Row";
+import Col from "react-bootstrap/lib/Row";
+import Grid from "react-bootstrap/lib/Grid";
 import "./Footer.css";
 const Footer = props => {
-    return (
-            <div>
-            <footer className="page-footer" style={{'padding':'20px'}}>
-                  <p> © 2018
-       
-               Created by:  <a className="footerlink" href="https://github.com/Komorebi-Girl">Stacy Jean-Philippe</a>, <a className="footerlink" href="#!">Priya Polla</a>, <a className="footerlink" href="#!">Kevin Hwang</a> & <a className="footerlink" href="https://github.com/elrizal">Erin Rizal</a>
+  return (
+    <div>
+      <footer className="page-footer" style={{ padding: "20px" }}>
+        <p>
+          {" "}
+          © 2018 Created by:{" "}
+          <a className="footerlink" href="https://github.com/Komorebi-Girl">
+            Stacy Jean-Philippe
+          </a>,{" "}
+          <a className="footerlink" href="#!">
+            Priya Polla
+          </a>,{" "}
+          <a className="footerlink" href="#!">
+            Kevin Hwang &amp;
+          </a>,{" "}
+          <a className="footerlink" href="https://github.com/elrizal">
+            Erin Rizal
+          </a>
         </p>
-          </footer>
-          </div>
-    );
-}
+      </footer>
+    </div>
+  );
+};
 export default Footer;
