325855161d2fe707abf21be6336bbf356a5a503b
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2018-03-17 21:43:30 -0400

    changed up static paths for logo

--- server.js
+++ server.js
@@ -32,32 +32,39 @@ app.use(bodyParser.json());
 // }
 
 // We need to use sessions to keep track of our user's login status
-app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Serve up the needed static files for each route
-app.use('/', express.static(path.join(__dirname, 'build/static')));
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, './build/', 'index.html'));
-  });
-app.use('/login', express.static(path.join(__dirname, 'build/static')));
-app.get('/login', function (req, res) {
-    res.sendFile(path.join(__dirname, './build/', 'index.html'));
-  });
-  
-app.use('/user/:id', express.static(path.join(__dirname, 'build/static')));
-app.get('/user/:id', function (req, res) {
-    res.sendFile(path.join(__dirname, './build/', 'index.html'));
-  });  
+app.use("/", express.static(path.join(__dirname, "build")));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.use('/nana/:id', express.static(path.join(__dirname, 'build/static')));
-app.get('/nana/:id', function (req, res) {
-    res.sendFile(path.join(__dirname, './build/', 'index.html'));
-  });
+//   app.use('/', express.static(path.join(__dirname, 'build/static')));
+//   app.get('/', function (req, res) {
+//       res.sendFile(path.join(__dirname, './build/', 'index.html'));
+//     });
+
+// app.use('/login', express.static(path.join(__dirname, 'build/static')));
+// app.get('/login', function (req, res) {
+//     res.sendFile(path.join(__dirname, './build/', 'index.html'));
+//   });
+
+// app.use('/user/:id', express.static(path.join(__dirname, 'build/static')));
+// app.get('/user/:id', function (req, res) {
+//     res.sendFile(path.join(__dirname, './build/', 'index.html'));
+//   });
+
+// app.use('/nana/:id', express.static(path.join(__dirname, 'build/static')));
+// app.get('/nana/:id', function (req, res) {
+//     res.sendFile(path.join(__dirname, './build/', 'index.html'));
+//   });
 
 app.use(routes);
-
 
 db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
